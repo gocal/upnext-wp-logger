c9ad43fabee1b459ba5938e25f8bb2a3398d9d4c
Marcin Marek Gocal <marcin.gocal@up-next.com>
2014-04-25 10:46:32 +0200

    added test project

---
+++ WpLogger.Wp8TestApp/WpLogger.Wp8TestApp/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WpLogger.Wp8TestApp.Resources;
+
+namespace WpLogger.Wp8TestApp
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
